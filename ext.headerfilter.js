46971402c49430d97b3489f81312878ef79d5e80
Danny Gould <danny@nowhere>
2013-06-05 17:03:01 +0100

    Initial commit

---
+++ ext.headerfilter.js
@@ -0,0 +1,260 @@
+(function ($) {
+    $.extend(true, window, {
+        "Ext": {
+            "Plugins": {
+                "HeaderFilter": HeaderFilter
+            }
+        }
+    });
+
+    function HeaderFilter(options) {
+        var grid;
+        var self = this;
+        var handler = new Slick.EventHandler();
+        var defaults = {
+            buttonImage: "../images/down.png",
+            filterImage: "../images/filter.png",
+            sortAscImage: "../images/sort-asc.png",
+            sortDescImage: "../images/sort-desc.png"
+        };
+        var $menu;
+
+        function init(g) {
+            options = $.extend(true, {}, defaults, options);
+            grid = g;
+            handler.subscribe(grid.onHeaderCellRendered, handleHeaderCellRendered)
+                   .subscribe(grid.onBeforeHeaderCellDestroy, handleBeforeHeaderCellDestroy)
+                   .subscribe(grid.onClick, handleBodyMouseDown)
+                   .subscribe(grid.onColumnsResized, columnsResized);
+
+            grid.setColumns(grid.getColumns());
+
+            $(document.body).bind("mousedown", handleBodyMouseDown);
+        }
+
+        function destroy() {
+            handler.unsubscribeAll();
+            $(document.body).unbind("mousedown", handleBodyMouseDown);
+        }
+
+        function handleBodyMouseDown(e) {
+            if ($menu && $menu[0] != e.target && !$.contains($menu[0], e.target)) {
+                hideMenu();
+            }
+        }
+
+        function hideMenu() {
+            if ($menu) {
+                $menu.remove();
+                $menu = null;
+            }
+        }
+
+        function handleHeaderCellRendered(e, args) {
+            console.log('handleHeaderCellRendered');
+            var column = args.column;
+
+            var $el = $("<div></div>")
+                          .addClass("slick-header-menubutton")
+                          .data("column", column)
+
+            if (options.buttonImage) {
+                $el.css("background-image", "url(" + options.buttonImage + ")");
+            }
+
+            $el.bind("click", showFilter).appendTo(args.node);
+        }
+
+        function handleBeforeHeaderCellDestroy(e, args) {
+            $(args.node)
+                .find(".slick-header-menubutton")
+                .remove();
+        }
+
+        function addMenuItem($menu, columnDef, title, command, image) {
+            var $item = $("<div class='slick-header-menuitem'>")
+                         .data("command", command)
+                         .data("column", columnDef)
+                         .bind("click", handleMenuItemClick)
+                         .appendTo($menu);
+
+            var $icon = $("<div class='slick-header-menuicon'>")
+                         .appendTo($item);
+
+            if (image) {
+                $icon.css("background-image", "url(" + image + ")");
+            }
+
+            $("<span class='slick-header-menucontent'>")
+             .text(title)
+             .appendTo($item);
+        }
+
+        function showFilter(e) {
+            var $menuButton = $(this);
+            var columnDef = $menuButton.data("column");
+
+            columnDef.filterValues = columnDef.filterValues || [];
+
+            var workingFilters = columnDef.filterValues.slice(0);
+
+            var filterItems;
+
+            if (workingFilters.length === 0) {
+                filterItems = getFilterValues(grid.getData(), columnDef);
+            }
+            else {
+                filterItems = getAllFilterValues(grid.getData().getItems(), columnDef);
+            }
+
+            if (!$menu) {
+                $menu = $("<div class='slick-header-menu'>").appendTo(document.body);
+            }
+
+            $menu.empty();
+
+            addMenuItem($menu, columnDef, 'Sort Ascending', 'sort-asc', options.sortAscImage);
+            addMenuItem($menu, columnDef, 'Sort Descending', 'sort-desc', options.sortDescImage);
+
+            var filterOptions = "<label><input type='checkbox' value='-1' />(Select All)</label>";
+
+            for (var i = 0; i < filterItems.length; i++) {
+                var filtered = _.contains(workingFilters, filterItems[i]);
+
+                filterOptions += "<label><input type='checkbox' value='" + i + "'"
+                                 + (filtered ? " checked='checked'" : "")
+                                 + "/>" + filterItems[i] + "</label>";
+            }
+
+            var $filter = $("<div class='filter'>")
+                           .append($(filterOptions))
+                           .appendTo($menu);
+
+            $('<button>OK</button>')
+                .appendTo($menu)
+                .bind('click', function (e) {
+                    columnDef.filterValues = workingFilters.splice(0);
+                    setButtonImage($menuButton, columnDef.filterValues.length > 0);
+                    handleApply(e, columnDef);
+                });
+
+            $('<button>Clear</button>')
+                .appendTo($menu)
+                .bind('click', function () {
+                    columnDef.filterValues.length = 0;
+                    setButtonImage($menuButton, false);
+                    handleApply(e, columnDef);
+                });
+
+            $('<button>Cancel</button>')
+                .appendTo($menu)
+                .bind('click', hideMenu);
+
+            $(':checkbox', $filter).bind('click', function () {
+                workingFilters = checkboxClick(filterItems, workingFilters, $(this));
+            });
+
+            var left = $(this).offset().left - $menu.width() + $(this).width() - 8;
+
+            $menu.css("top", $(this).offset().top + $(this).height())
+                 .css("left", (left > 0 ? left : 0));
+        }
+
+        function columnsResized() {
+            hideMenu();
+        }
+
+        function checkboxClick(filterItems, workingFilters, $checkbox) {
+            var value = $checkbox.val();
+            var $filter = $checkbox.parent().parent();
+
+            if ($checkbox.val() < 0) {
+                if ($checkbox.prop('checked')) {
+                    $(':checkbox', $filter).prop('checked', true);
+                    workingFilters = filterItems.slice(0);
+                } else {
+                    $(':checkbox', $filter).prop('checked', false);
+                    workingFilters.length = 0;
+                }
+            } else {
+                var index = _.indexOf(workingFilters, filterItems[value]);
+
+                if ($checkbox.prop('checked') && index < 0) {
+                    workingFilters.push(filterItems[value]);
+                }
+                else {
+                    if (index > -1) {
+                        workingFilters.splice(index, 1);
+                    }
+                }
+            }
+
+            return workingFilters;
+        }
+
+        function setButtonImage($el, filtered) {
+            var image = "url(" + (filtered ? options.filterImage : options.buttonImage) + ")";
+
+            $el.css("background-image", image);
+        }
+
+        function handleApply(e, columnDef) {
+            hideMenu();
+
+            self.onFilterApplied.notify({ "grid": grid, "column": columnDef }, e, self);
+
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        function getFilterValues(dataView, column) {
+            var seen = [];
+            for (var i = 0; i < dataView.getLength(); i++) {
+                var value = dataView.getItem(i)[column.field];
+
+                if (!_.contains(seen, value)) {
+                    seen.push(value);
+                }
+            };
+
+            return _.sortBy(seen, function (v) { return v; });
+        }
+
+        function getAllFilterValues(data, column) {
+            var seen = [];
+            for (var i = 0; i < data.length; i++) {
+                var value = data[i][column.field];
+                if (!_.contains(seen, value)) {
+                    seen.push(value);
+                }
+            }
+
+            return _.sortBy(seen, function (v) { return v; });
+        }
+
+        function handleMenuItemClick(e) {
+            var command = $(this).data("command");
+            var columnDef = $(this).data("column");
+
+            hideMenu();
+
+            if (command != null && command != '') {
+                self.onCommand.notify({
+                    "grid": grid,
+                    "column": columnDef,
+                    "command": command
+                }, e, self);
+            }
+
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        $.extend(this, {
+            "init": init,
+            "destroy": destroy,
+            "onFilterApplied": new Slick.Event(),
+            "onCommand": new Slick.Event()
+        });
+    }
+})(jQuery);
---
+++ ext.overlays.js
@@ -0,0 +1,363 @@
+﻿(function ($) {
+    $.extend(true, window, {
+        "Ext": {
+            "Plugins": {
+                "Overlays": Overlays
+            }
+        }
+    });
+
+    function Overlays(options) {
+        var $headerOverlay;
+        var $rowOverlay;
+        var selectionOverlay;
+
+        var currentColumn;
+        var handleDragging;
+        var grid;
+        var self = this;
+        var handler = new Slick.EventHandler();
+        var dragDecorator;
+
+        var defaults = {
+            buttonCssClass: null,
+            buttonImage: "../images/down.gif"
+        };
+
+        function init(g) {
+            options = $.extend(true, {}, defaults, options);
+            grid = g;
+
+            dragDecorator = new overlayRangeDecorator(grid);
+
+            $headerOverlay = createRowHeaderOverlay(1200);
+            $rowOverlay = createRowHeaderOverlay(500);
+            selectionOverlay = createSelectionOverlay();
+
+            handler.subscribe(grid.onActiveCellChanged, activeCellChanged)
+                   .subscribe(grid.onColumnsResized, columnResized)
+                   .subscribe(grid.onScroll, gridScrolled);
+        }
+
+        function destroy() {
+            handler.unsubscribeAll();
+            selectionOverlay.$handle.unbind("mousedown", handleBodyMouseDown)
+                                    .unbind('drag', handleOverlayDrag)
+                                    .unbind('dragend', handleOverlayDragEnd);
+        }
+
+        function createRowHeaderOverlay(zIndex) {
+            return $('<div>')
+                    .addClass("header-overlay")
+                    .css("position", "absolute")
+                    .css("z-index", zIndex)
+                    .appendTo('body');
+        }
+
+        function createSelectionOverlay() {
+            var canvas = grid.getCanvasNode();
+            var selectionOverlay = new Overlay(canvas);
+
+            selectionOverlay
+              .$handle
+              .bind('dragstart', handleOverlayDragStart)
+              .bind('drag', handleOverlayDrag)
+              .bind('dragend', handleOverlayDragEnd);
+
+            return selectionOverlay;
+        }
+
+        function activeCellChanged(e, args) {
+            dragDecorator.hide();
+            
+            moveSelectionOverlay(e, args);
+            moveHeaderRowOverlays(e, args);
+        }
+
+        function columnResized(e, args) {
+            moveHeaderRowOverlays(e, args);
+            moveSelectionOverlay(e, args);
+        }
+
+        function gridScrolled(e, args) {
+            moveHeaderRowOverlays(e, args);
+            moveSelectionOverlay(e, args);
+        }
+
+        function moveHeaderRowOverlays(e, args) {
+            if (typeof args.cell != 'undefined') {
+                currentColumn = args.cell;
+            } else {
+                if (!currentColumn) {
+                    return;
+                }
+            }
+
+            if (!grid.getActiveCell()) {
+                $rowOverlay.hide();
+                return;
+            }
+
+            $rowOverlay.show();
+
+            var column = grid.getColumns()[currentColumn];
+
+            $(".slick-header > div > div")
+                .removeClass("selected-header");
+
+            $('[id$=' + column.id + ']', '.slick-header')
+                .addClass('selected-header');
+
+            var headerHeight = $('.slick-header').height();
+            var cellPosition = grid.getActiveCellPosition();
+            var gridPosition = grid.getGridPosition();
+
+            $rowOverlay.toggle(cellPosition.visible);
+            $headerOverlay.toggle(cellPosition.visible);
+
+            var headerWidth = Math.min(cellPosition.width,
+                                       gridPosition.width - cellPosition.left + 4);
+
+            $headerOverlay.css({
+                left: cellPosition.left,
+                top: gridPosition.top + headerHeight - 2,
+                width: headerWidth,
+                height: 2
+            });
+
+            $rowOverlay.css({
+                left: gridPosition.left,
+                top: cellPosition.top,
+                width: 2,
+                height: cellPosition.height
+            });
+        }
+
+        function moveSelectionOverlay(e, args) {
+            var activeCell = grid.getActiveCell();
+
+            if (!activeCell) {
+                selectionOverlay.hide();
+                return;
+            }
+
+            selectionOverlay.show();
+
+            var position = grid.getCellNodeBox(activeCell.row, activeCell.cell);
+
+            // Not coming through on the property so re-calculated
+            position.height = position.bottom - position.top;
+            position.width = position.right - position.left;
+
+            selectionOverlay.$left.css({
+                left: position.left - 2,
+                top: position.top,
+                width: 2,
+                height: position.height
+            });
+
+            selectionOverlay.$right.css({
+                left: position.left + position.width - 1,
+                top: position.top,
+                width: 2,
+                height: position.height
+            });
+
+            selectionOverlay.$top.css({
+                left: position.left - 2,
+                top: position.top - 2,
+                width: position.width + 3,
+                height: 2
+            });
+
+            selectionOverlay.$bottom.css({
+                left: position.left - 2,
+                top: position.top + position.height - 1,
+                width: position.width + 3,
+                height: 2
+            });
+
+            selectionOverlay.$handle.css({
+                left: position.left + position.width - 4,
+                top: position.top + position.height - 4,
+                width: 1,
+                height: 1
+            });
+        }
+
+        function handleOverlayDragStart(e, dd) {
+            var canvas = grid.getCanvasNode();
+
+            var cell = grid.getActiveCell();
+
+            if (grid.canCellBeSelected(cell.row, cell.cell)) {
+                handleDragging = true;
+                e.stopImmediatePropagation();
+            }
+
+            if (!handleDragging) {
+                return;
+            }
+
+            grid.focus();
+
+            dd.range = { start: cell, end: {} };
+
+            $(this).css({
+                "background-color": "transparent",
+                "border-color": "transparent"
+            });
+
+            return dragDecorator.show(new Slick.Range(cell.row, cell.cell));
+        }
+
+        function handleOverlayDrag(e, dd) {
+            if (!handleDragging) {
+                return;
+            }
+
+            var canvas = grid.getCanvasNode();
+
+            e.stopImmediatePropagation();
+
+            var end = grid.getCellFromPoint(
+                e.pageX - $(canvas).offset().left,
+                e.pageY - $(canvas).offset().top);
+
+            if (!grid.canCellBeSelected(end.row, end.cell)) {
+                return;
+            }
+
+            dd.range.end = end;
+
+            dragDecorator.show(new Slick.Range(dd.range.start.row,
+                                               dd.range.start.cell,
+                                               end.row,
+                                               dd.range.start.cell));
+        }
+
+        function handleOverlayDragEnd(e, dd) {
+            if (!handleDragging) {
+                return;
+            }
+
+            handleDragging = false;
+
+            $(this).css({
+                "background-color": "",
+                "border-color": ""
+            });
+
+            dragDecorator.hideHandle();
+
+            self.onFillUpDown.notify({ "grid": grid, "range": dragDecorator.getSelectedRange() }, e, self);
+
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        function Overlay(target) {
+            this.$left = $('<div>').addClass('header-overlay').appendTo(target);
+            this.$right = $('<div>').addClass('header-overlay').appendTo(target);
+            this.$top = $('<div>').addClass('header-overlay').appendTo(target);
+            this.$bottom = $('<div>').addClass('header-overlay').appendTo(target);
+            this.$handle = $('<div>').addClass("handle-overlay").appendTo(target);
+
+            this.show = function () {
+                this.$left.show();
+                this.$right.show();
+                this.$top.show();
+                this.$bottom.show();
+                this.$handle.show();
+            }
+
+            this.hide = function () {
+                this.$left.hide();
+                this.$right.hide();
+                this.$top.hide();
+                this.$bottom.hide();
+                this.$handle.hide();
+            }
+        }
+
+        function overlayRangeDecorator(grid) {
+            var decorator;
+            var r;
+
+            function show(range) {
+                r = range;
+                if (!decorator) {
+                    decorator = new Overlay(grid.getCanvasNode());
+                }
+
+                var from = grid.getCellNodeBox(range.fromRow, range.fromCell);
+                var to = grid.getCellNodeBox(range.toRow, range.toCell);
+
+                decorator.$left.css({
+                    top: from.top - 2,
+                    left: from.left - 2,
+                    height: to.bottom - from.top + 2,
+                    width: 2
+                });
+
+                decorator.$right.css({
+                    top: from.top - 2,
+                    left: to.right - 1,
+                    height: to.bottom - from.top + 2,
+                    width: 2
+                });
+
+                decorator.$top.css({
+                    top: from.top - 2,
+                    left: to.left - 1,
+                    height: 2,
+                    width: to.right - from.left + 2
+                });
+
+                decorator.$bottom.css({
+                    top: to.bottom - 1,
+                    left: from.left - 2,
+                    height: 2,
+                    width: to.right - from.left + 3
+                });
+
+                decorator.$handle.css({
+                    top: to.bottom - 3,
+                    left: from.right - 4,
+                    height: 1,
+                    width: 1
+                });
+
+                return decorator;
+            }
+
+            function getSelectedRange() {
+                return r;
+            }
+
+            function hide() {
+                if (decorator) {
+                    decorator.hide();
+                    decorator = null;
+                }
+            }
+
+            function hideHandle() {
+                decorator.$handle.hide();
+            }
+
+            return { 
+                     hide: hide,
+                     hideHandle: hideHandle,
+                     show: show,
+                     getSelectedRange: getSelectedRange
+            };
+        }
+
+        $.extend(this, {
+            "init": init,
+            "destroy": destroy,
+            "onFillUpDown": new Slick.Event()
+        });
+    }
+})(jQuery);
